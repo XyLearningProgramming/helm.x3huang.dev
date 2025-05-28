dc4a20e2339c131beaebcf35ea66c94d8c6ef847
Xinyu Huang <47282888+XyLearningProgramming@users.noreply.github.com>
2025-05-28 18:06:06 +0800

    ✨ feat/dashboard (#7)
    * 🐛 minor changes to README
    
    * ✨ added prom stack for kubelet and traefik
    
    ---------
    
    Co-authored-by: XyLearningProgramming <XyLearningProgramming@users.noreply.github.com>

---
+++ scripts/apply-secrets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+# Enter the directory where the script is located
+cd "$(dirname "$0")/.."
+export WORKSPACE="$(pwd)"
+
+for file in ./secrets/*.env; do
+  # Ignore *.example.env
+  if [[ "$file" == *.example.env ]]; then
+    continue
+  fi
+  ns="${file##*/}"         # Strip path
+  ns="${ns%.env}"          # Remove .env extension to get namespace
+  kubectl create secret generic helmfile-secret \
+    --namespace "$ns" \
+    --dry-run=client \
+    --from-env-file="$file" \
+    -o yaml | kubectl apply -f -
+done
