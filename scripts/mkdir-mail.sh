1830e5bf95efcd9a5374f482e8cfa4da8462e990
Xinyu Huang <contact@x3huang.dev>
2025-07-02 10:12:45 +0800

    🐛 fixed mail config wiped after reboot (#29)
    Co-authored-by: XyLearningProgramming <XyLearningProgramming@users.noreply.github.com>

--- scripts/mkdir-mail.sh
+++ scripts/mkdir-mail.sh
@@ -5,13 +5,13 @@ set -ex
 # One-time script to execute on EMAIL_NODE to make directory 
 # necessary for mailserver to mount.
 
-mkdir -p /tmp/docker-mailserver
+mkdir -p /var/docker-mailserver
 mkdir -p /var/mail
 mkdir -p /var/mail-state
 mkdir -p /var/log/mail
 mkdir -p /usr/local/pgsql/data
 
-chown -R 1000:1000 /tmp/docker-mailserver
+chown -R 1000:1000 /var/docker-mailserver
 chown -R 1000:1000 /var/mail
 chown -R 1000:1000 /var/mail-state
 chown -R 1000:1000 /var/log/mail
