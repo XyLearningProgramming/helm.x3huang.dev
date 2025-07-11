6bb23358aa111a04e5fa7ff5acdb387147ddbfc0
XyLearningProgramming <XyLearningProgramming@users.noreply.github.com>
2025-07-11 18:09:22 +0800

    ✨ added independent redis chart

--- scripts/mkdir-mail.sh
+++ scripts/mkdir-mail.sh
@@ -10,9 +10,11 @@ mkdir -p /var/mail
 mkdir -p /var/mail-state
 mkdir -p /var/log/mail
 mkdir -p /usr/local/pgsql/data
+mkdir -p /usr/local/redis/data
 
 chown -R 1000:1000 /var/docker-mailserver
 chown -R 1000:1000 /var/mail
 chown -R 1000:1000 /var/mail-state
 chown -R 1000:1000 /var/log/mail
 chown -R 1000:1000 /usr/local/pgsql/data
+chown -R 1000:1000 /usr/local/redis/data
